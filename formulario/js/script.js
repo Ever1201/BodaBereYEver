75553ee71599fd5c575c631f2acebad65bc3a2f9
Ever Gerardo Garcia Botello <gerardo.egb@gmail.com>
2025-04-29 20:15:15 -0600

    formulario

---
+++ formulario/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("invitationForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("¡Formulario enviado! Gracias por tu información. Nos pondremos en contacto pronto.");
+});
